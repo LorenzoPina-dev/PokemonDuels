517a7c7aa4ba32b604d5626a8dcec45c837afd66
LorenzoPina-dev <lory.pina.03@gmail.com>
2022-01-13 20:25:27 +0100

    Correzzione background routa

--- pokemonDuel/pokemonDuel/classi/Logicagioco/Ruota.cs
+++ pokemonDuel/pokemonDuel/classi/Logicagioco/Ruota.cs
@@ -94,7 +94,7 @@ namespace pokemonDuel.classi.Logicagioco
                 if (Pokemon != null)
                 {
                     ultimoAngolo =(ultimoAngolo+ Gradi)%360;
-                    g.Clear(Color.White);
+                    g.Clear(Color.FromArgb(255, 120, 120, 120));
                     int Arrivati = ultimoAngolo;
                     for (int i = 0; i < _pokemon.Mosse.Count; i++)
                     {
